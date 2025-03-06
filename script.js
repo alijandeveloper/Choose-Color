c11133b9b118e73dc7ce26b7d7bda48acd0e75d1
Ali Jan <alijandeveloper15@gmail.com>
2025-03-06 10:32:37 -0800

    Get Element SCRIPT

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const colorPicker = document.getElementById("colorPicker");
+    const hexCode = document.getElementById("hexCode");
+    const rgbCode = document.getElementById("rgbCode");
+    const hslCode = document.getElementById("hslCode");
+    const randomColorBtn = document.getElementById("randomColor");
+    const colorHistory = document.getElementById("colorHistory");
+    const colorShades = document.getElementById("colorShades");
+    const copyButtons = document.querySelectorAll(".copy-btn");
